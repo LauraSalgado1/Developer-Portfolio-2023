4c11cc88cecd8f7557e9cc27e44b34aaacd12175
Laura Culham <culham.laura@gmail.com>
2023-03-28 16:19:29 -0400

    add nuxt modules

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Developer-Portfolio-2023
+
+https://laurasalgado.com
+
+## nuxt3
+
+https://nuxt.com/
+
+## modules usage
+
+https://nuxt.com/modules/swiper
+https://nuxt.com/modules/seo-kit
+https://nuxt.com/modules/vue-transitions#usage-with-nuxt
+https://v1.image.nuxtjs.org/get-started
+https://nuxt.com/modules/icon
+
+## plugins usage
+
+https://animejs.com/
--- laura-website-2023/nuxt.config.ts
+++ laura-website-2023/nuxt.config.ts
@@ -14,4 +14,28 @@ export default defineNuxtConfig({
       },
     },
   },
+  modules: [
+    [
+      "nuxt-swiper",
+      {
+        // Swiper options
+      },
+    ],
+    "@morev/vue-transitions/nuxt",
+    "@nuxt/image-edge",
+    "nuxt-icon",
+  ],
+  vueTransitions: {
+    // The same options as in the plugin itself.
+    // You will get an autocomplete using Nuxt 3.
+  },
+  extends: ["nuxt-seo-kit"],
+  runtimeConfig: {
+    public: {
+      siteUrl: "https://laurasalgado.com",
+      siteName: "Laura Salgado | Developer",
+      siteDescription: "add site desc here",
+      language: "en-CA",
+    },
+  },
 });
